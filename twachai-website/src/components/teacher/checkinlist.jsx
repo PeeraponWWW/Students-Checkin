ed8793c8b657295980bfa512506a61819464091c
tijo <81065469+artijo@users.noreply.github.com>
2024-03-05 17:08:58 +0700

    show std checked

--- twachai-website/src/components/teacher/checkinlist.jsx
+++ twachai-website/src/components/teacher/checkinlist.jsx
@@ -1,4 +1,38 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+export const ShowDetail = ({data}) => {
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>ลำดับ</th>
+                    <th>รหัสนักศึกษา</th>
+                    <th>ชื่อ</th>
+                    <th>เวลาเข้า</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((detail, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{detail.std_id? detail.std_id : ""}</td>
+                            <td>{detail.name? detail.name : ""}</td>
+                            <td>{detail.checked_date? detail.checked_date.toDate().toLocaleTimeString(): ""}</td>
+                        </tr>
+                    )
+                }
+                )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
 export default function CheckinList({...props}) {
+    const [stdChecked, setStdChecked] = useState([])
     return (
         <div>
             <h1>CheckinList</h1>
@@ -21,12 +55,18 @@ export default function CheckinList({...props}) {
                             <td>{checkin.room}</td>
                             <td>{checkin.id}</td>
                             <td>{checkin.class_date.toDate().toLocaleDateString()}</td>
+                            <td>
+                                <Button onClick={() => {
+                                    setStdChecked(checkin.checked)
+                                }}>ดูรายชื่อ</Button>
+                            </td>
                         </tr>
                     )
                 }
                 )}
                 </tbody>
             </table>
+            {stdChecked && <ShowDetail data={stdChecked}/>}
         </div>
     );
 }
